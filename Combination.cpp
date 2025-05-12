855f71a7ec6dd181f338c27bdd734a5b01e601c4
Nminhtruyet1205 <nminhtruyet4869@gmail.com>
2025-05-13 00:30:52 +0700

    Add files via upload

---
+++ Combination.cpp
@@ -0,0 +1,61 @@
+#include "Combination.h"
+#include <iostream>
+
+using namespace std;
+
+Combination::Combination() : ans(0), preans(0) {}
+
+int Combination::factorial(int n) {
+    int result = 1;
+    for (int i = 2; i <= n; ++i)
+        result *= i;
+    return result;
+}
+
+double Combination::parseInput(string& input) {
+    if (input == "mem") return mem;
+    if (input == "result") return result;
+    double value;
+    cin>>value;
+    return value;
+}
+
+void Combination::welcome() const {
+    cout << "Welcome to Combination Mode!" << endl;
+}
+
+void Combination::parseOperation(string& op) {
+    if (op == "print_mem") printMem();
+    else if (op == "set_mem") setMem();
+    else cout << "Unknown operation.\n";
+}
+
+void Combination::combi_cal(int n, int r) {
+    preans = ans;
+    ans = factorial(n) / (factorial(r) * factorial(n - r));
+    result = ans;
+    cout << "C(" << n << ", " << r << ") = " << ans << endl;
+}
+
+void Combination::arrangement(int n, int r) {
+    preans = ans;
+    ans = factorial(n) / factorial(n - r);
+    result = ans;
+    cout << "A(" << n << ", " << r << ") = " << ans << endl;
+}
+
+void Combination::permutation(int n) {
+    preans = ans;
+    ans = factorial(n);
+    result = ans;
+    cout << "P(" << n << ") = " << ans << endl;
+}
+
+void Combination::setMem() {
+    mem = result;
+    cout << "Memory set to: " << mem << endl;
+}
+
+void Combination::printMem() const {
+    cout << "Memory value: " << mem << endl;
+}
---
+++ Combination.h
@@ -0,0 +1,29 @@
+#ifndef COMBINATION_H
+#define COMBINATION_H
+
+#include "calculator.h"
+#include <string>
+using namespace std;
+class Combination : public Calculator {
+private:
+    int ans;
+    int preans;
+
+    int factorial(int n);
+
+public:
+    Combination();
+    virtual ~Combination() = default;
+
+    double parseInput(string& input) ;
+    void welcome() ;
+    void parseOperation(string& op) ;
+
+    void combi_cal(int n, int r);
+    void arrangement(int n, int r);
+    void permutation(int n);
+
+    void setMem();         
+    void printMem() ; 
+};
+#endif
