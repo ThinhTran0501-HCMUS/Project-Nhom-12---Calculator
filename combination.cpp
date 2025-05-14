3318d511c352066a1f8d1879880953003c5455cc
N.T.V Thịnh <148090637+vanyYeager@users.noreply.github.com>
2025-05-14 15:57:25 +0700

    Base

--- combination.cpp
+++ combination.cpp
@@ -1,66 +1,61 @@
-#include<iostream>
-#include<math.h>
+#include "Combination.h"
+#include <iostream>
+
 using namespace std;
 
-class Calculator{
-    public:
-    long long factorial(int n);
-    long long combination(int n ,int k);
-    long long arrangement(int n, int k);
+Combination::Combination() : ans(0), preans(0) {}
 
-};
-    long long Calculator::factorial(int n){ // hàm tinh giai thua cua n phan tu 
-        if(n==0){
-            return 1;
-        }
-        else {
-            return n*factorial(n-1);
-        }
-    }
-    long long Calculator::combination(int n, int k){ // ham tinh to hop chap k cua n phan tu 
-        if(n>1 && n>k && k>0){
-            return factorial(n)/(factorial(k)*factorial(n-k));
-        }
-        else{
-            cout << "error" <<endl;
-            return -1;
-        }
-    }
-    long long Calculator::arrangement(int n, int k){ // ham tinh chinh hop chap k cua n phan tu
-        if(n>1 && n>k && k>0){
-            return factorial(n)/(factorial(n-k));
-        }
-        else {
-            cout << "error" <<endl;
-            return -1;
-        }
-    }
-    int main(){
-        Calculator hv_th_ch;
-        int n;
-        int k;
-        int option;
-        cout <<" entered n elements "<<endl;
-        cin>>n;
-        cout<<" entered k elements "<<endl;
-        cin>>k;
-        cout << "\n option :\n";
-        cout << "1. combination (C)\n";
-        cout << "2. arrangement (A)\n";
-        cin>>option;
-        cout << "--------------------------\n";
-        switch (option) {
-            case 1:
-                if(n >= k && n >= 1 && k >= 0){
-                cout << "combination C(" << n << ", " << k << ") = " << hv_th_ch.combination(n, k) << endl;}
-                break;
-            case 2:
-                if(n >= k && n >= 1 && k >= 0){
-                cout << "arrangement A(" << n << ", " << k << ") = " << hv_th_ch.arrangement(n, k) << endl;}
-                break;
-            default:
-                cout << "error!\n";
-                break;
-        }
-        return 0;
-    }
+int Combination::factorial(int n) {
+    int result = 1;
+    for (int i = 2; i <= n; ++i)
+        result *= i;
+    return result;
+}
+
+double Combination::parseInput(string& input) {
+    if (input == "mem") return mem;
+    if (input == "result") return result;
+    double value;
+    cin>>value;
+    return value;
+}
+
+void Combination::welcome()  {
+    cout << "Welcome to Combination Mode!" << endl;
+}
+
+void Combination::parseOperation(string& op) {
+    if (op == "print_mem") printMem();
+    else if (op == "set_mem") setMem();
+    else cout << "Unknown operation.\n";
+}
+
+void Combination::combi_cal(int n, int r) {
+    preans = ans;
+    ans = factorial(n) / (factorial(r) * factorial(n - r));
+    result = ans;
+    cout << "C(" << n << ", " << r << ") = " << ans << endl;
+}
+
+void Combination::arrangement(int n, int r) {
+    preans = ans;
+    ans = factorial(n) / factorial(n - r);
+    result = ans;
+    cout << "A(" << n << ", " << r << ") = " << ans << endl;
+}
+
+void Combination::permutation(int n) {
+    preans = ans;
+    ans = factorial(n);
+    result = ans;
+    cout << "P(" << n << ") = " << ans << endl;
+}
+
+void Combination::setMem() {
+    mem = result;
+    cout << "Memory set to: " << mem << endl;
+}
+
+void Combination::printMem() {
+    cout << "Memory value: " << mem << endl;
+}
