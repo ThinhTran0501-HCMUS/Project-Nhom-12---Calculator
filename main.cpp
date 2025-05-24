cbc55ec51e938460da2da61681471f30590ed3d0
N.T.V Thịnh <148090637+vanyYeager@users.noreply.github.com>
2025-05-24 18:14:53 +0700

    24/5

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include "Basic_calculator.h"
 #include "iostream"
 int main(){
-    Basic_calculator* b;
+
+    Basic_calculator* b=new Basic_calculator;
     std::string input="" ;
-     b->welcome();
-    while (std::cin>>input &&std::cout<<"\n>>> "&&true ){
+    b->welcome();
+    while (std::cout<<"\n>>> "&&std::cin>>input &&true ){
        if (input=="Exit"||input=="exit"){
         std::cout<<"Exit basic calculator"<<std::endl;
         break;
