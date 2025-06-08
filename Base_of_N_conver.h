f2f98ef8c4f7f98bc5bbfca819e0b9ecfc4c8b5a
Tuan Khoa Tran Ngo <23130024@student.hcmus.edu.vn>
2025-06-08 23:53:34 +0700

    Editing...

--- Base_of_N_conver.h
+++ Base_of_N_conver.h
@@ -1,32 +1,41 @@
 #ifndef BASE_H
 #define BASE_H
 
-#include<iostream>
-
-#include<vector>
-#include <string.h>
-#include <math.h>
+#include "Calculator_mode.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <cmath>
 
 using namespace std;
-const char digit[]="0123456789ABCDEF";
-class Base{
-    private:
-    string bin,dec,oct,hex;
-    string input;
-    public:
-    Base(int choice);
 
-    //hàm nhập bin,dec,hex,oct
+const char digit[] = "0123456789ABCDEF";
+
+class Base : public Calculator_mode {
+private:
+    int choice;
+    string modeName;
+    string bin, dec, oct, hex, input;
+
     void setBin();
     void setDec();
     void setHex();
     void setOct();
 
-    int To_Dec(string input, int base_conver);// cơ số bất kỳ ngược lại 10
-
-    string To_BaseOfN(int number, int base);//cơ số 10 -> base bất kỳ
-
+    int To_Dec(string input, int base);
+    string To_BaseOfN(int number, int base);
     bool isValidForBase(const string& input, int base);
+
+public:
+    Base();
+    virtual ~Base() = default;
+
+    virtual void welcome() override;
+    virtual void parse_operation(string const& str) override;
+    virtual void print_name() override;
+    virtual void set_choice(int choice) override;
+    virtual int get_choice() override;
 
     void display();
 };
